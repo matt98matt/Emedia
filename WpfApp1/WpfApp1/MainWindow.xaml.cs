eac21a918d1de1ac412f529cd02ec2bbff67129a
Mateusz <atthew98jura@gmail.com>
2019-03-28 22:37:54 +0100

    firs functionality

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -56,13 +56,73 @@ namespace WpfApp1
         private void ToArray()
         {
             byte[] image = System.IO.File.ReadAllBytes(ImageSource);
+            
+            int dana = 0;
+
+            for(int i = 0; i<(image.Length-1); i++ ){
+                if(image[i] == 0xff && (image[i+1] == 0xC0 || image[i+1] == 0xC2)){
+                dana = i;
+                    }
+            }
+
+            byte[] image2 = new byte[image[dana + 3]];
+            for(int i = 0, j =0; i<(image2.Length); i++, j++){
+                image2[j] = image[i + dana + 2];
+}
+            int width = 0;
+            int height = 0;
+            int bitDepth = 0;
+            string numberOfComponents = "";
+
+            height = image2[3]*16*16 + image2[4];
+            width = image2[5]*16*16 + image2[6];
+            bitDepth = image2[2];
+
+            switch(image2[7]){
+                case 0x01:
+                numberOfComponents = "monochromatyczny";
+                break;
+                case 0x03:
+                numberOfComponents = "color YcbCr or YIQ";
+                break;
+                case 0x04:
+                numberOfComponents = "CMYK";
+                break;
+                deafault:
+                break;
+       }
+            TextBox jpgWidth = new TextBox();
+            jpgWidth.Text = "Width: " + width + "px";
+            jpgWidth.Margin = new System.Windows.Thickness(300, 0, 0, 0);
+            Wrapper.Children.Add(jpgWidth);
+
+            TextBox jpgHeight = new TextBox();
+            jpgHeight.Text = "Height: " + height + "px";
+            jpgHeight.Margin = new System.Windows.Thickness(300, 50, 0, 0);
+            Wrapper.Children.Add(jpgHeight);
+
+            TextBox jpgDepth = new TextBox();
+            jpgDepth.Text = "Bit depth: " + bitDepth;
+            jpgDepth.Margin = new System.Windows.Thickness(300, 100, 0, 0);
+            Wrapper.Children.Add(jpgDepth); 
+
+            TextBox jpgType = new TextBox();
+            jpgType.Text = "Type: " + numberOfComponents;
+            jpgType.Margin = new System.Windows.Thickness(300, 150, 0, 0);
+            Wrapper.Children.Add(jpgType);
+            
+            //Width = width;
+            //Height = height;
+            //BitDepth = bitDepth;
+            //Type = numberOfComponents;
+
             WriteToFile(image);
         }
         // https://stackoverflow.com/questions/772388/c-sharp-how-can-i-test-a-file-is-a-jpeg ??
         private void WriteToFile(byte[] image)
         {
             string path = @"C:\Users\micha\Desktop\E-Media\Images\test.txt";
-            File.WriteAllBytes(path,image);
+            //File.WriteAllBytes(path,image);
         }
     }
 }
